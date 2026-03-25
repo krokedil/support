78064605fdbc01c47b15b7b58715bb83efed16e8
anyakro <anya@krokedil.se>
2026-03-25 15:01:03 +0100

    Update AJAX call

--- assets/js/admin-order.js
+++ assets/js/admin-order.js
@@ -1,4 +1,10 @@
 jQuery(function ($) {
+    	"use strict"
+
+	if ( krokedil_support_admin_order_params === undefined ) {
+		return false
+	}
+
     const adminOrder = {
         init: function () {
             this.bindEvents();
@@ -12,34 +18,42 @@ jQuery(function ($) {
             e.preventDefault();
             const orderId = krokedil_support_admin_order_params.order_id;
             const { url, nonce } = krokedil_support_admin_order_params.ajax.export_order;
-            const data = {
-                action: 'krokedil_support_order_action',
-                order_id: orderId,
-                nonce: nonce
-            };
 
-            $.post(url, data, function (response) {
-                if (response.success) {
-                    // Download the response as a file.
-                    try{
-                        const jsonString = JSON.stringify(response.data, null, 2);
-                        const blob = new Blob([jsonString], { type: 'application/json' });
-                        const url = URL.createObjectURL(blob);
-                        const a = document.createElement('a');
-                        const unixTimestamp = Math.floor(Date.now() / 1000);
-                        a.href = url;
-                        a.download = `${orderId}_${unixTimestamp}.json`;
-                        document.body.appendChild(a);
-                        a.click();
-                        window.URL.revokeObjectURL(url);
+            $.ajax( {
+				type: "POST",
+				data: {
+					nonce: nonce,
+					order_id: orderId,
+				},
+				dataType: "JSON",
+				url: url,
+				success: function ( response ) {
+                    if (response.success) {
+                        // Download the response as a file.
+                        try{
+                            const jsonString = JSON.stringify(response.data, null, 2);
+                            const blob = new Blob([jsonString], { type: 'application/json' });
+                            const url = URL.createObjectURL(blob);
+                            const a = document.createElement('a');
+                            const unixTimestamp = Math.floor(Date.now() / 1000);
+                            a.href = url;
+                            a.download = `${orderId}_${unixTimestamp}.json`;
+                            document.body.appendChild(a);
+                            a.click();
+                            window.URL.revokeObjectURL(url);
+                        }
+                        catch(e){
+                            console.error(e);
+                        }
+                    } else {
+                        console.error(response.data);
                     }
-                    catch(e){
-                        console.error(e);
-                    }
-                } else {
-                    console.error(response.data);
-                }
-            });
+
+				},
+				error: function ( response ) {
+					console.error(response.data);
+				},
+			} )
         }
     }
 
